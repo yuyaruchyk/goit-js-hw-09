0a31a4f272bb0175ec393cb756feaff7e1eb1d92
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-14 00:12:12 +0200

    upd
    upd

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,7 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const selectors = {
-    startBtn: document.querySelector('[data-start]'),
+  startBtn: document.querySelector('[data-start]'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
@@ -11,72 +11,58 @@ const selectors = {
 
 const flatpickr = require('flatpickr');
 flatpickr('#datetime-picker', {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) { 
-        console.log(selectedDates[0]);
-        const currentDate = new Date();
-        if (selectedDates[0] < currentDate) {
-            window.alert("Please choose a date in the future");
-            selectors.startBtn.disabled = true;
-
-        } else {
-            selectors.startBtn.disabled = false;
-
-
-        }
-const timer = {
-    start() {
-        
-        setInterval(() => {
-            const startTime = selectedDates[0];
-            const finishTime = Date.now();
-            const deltaTime = startTime - finishTime;
-            const { days, hours, minutes, seconds }
-             = getTimeComponents(deltaTime);
-            selectors.days.textContent = days;
-            selectors.hours.textContent = hours;
-            selectors.minutes.textContent = minutes;
-            selectors.seconds.textContent = seconds;
-            
-
-        }, 1000);
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    const currentDate = new Date();
+    if (selectedDates[0] < currentDate) {
+      window.alert('Please choose a date in the future');
+      selectors.startBtn.disabled = true;
+    } else {
+      selectors.startBtn.disabled = false;
     }
+      const timer = {
+          
+      start() {
+        setInterval(() => {
+          const startTime = selectedDates[0];
+          const finishTime = Date.now();
+          const deltaTime = startTime - finishTime;
+          const { days, hours, minutes, seconds } =
+            getTimeComponents(deltaTime);
+          selectors.days.textContent = days;
+          selectors.hours.textContent = hours;
+          selectors.minutes.textContent = minutes;
+          selectors.seconds.textContent = seconds;
+        }, 1000);
+      },
+    };
 
-    
-        };
-        
-
-        selectors.startBtn.addEventListener("click", () => {
-            timer.start();
-        });
+    selectors.startBtn.addEventListener('click', () => {
+        timer.start();
+        if (selectors.days.textContent === "00" && selectors.hours.textContent === "00" && selectors.minutes.textContent === "00" && selectors.seconds.textContent === "00") {
+            return;
         }
+    });
+      
+  },
 });
 function pad(value) {
-    return String(value).padStart(2, '0');
+  return String(value).padStart(2, '0');
 }
 function getTimeComponents(time) {
-    const milliseconds = Math.abs(time);
-    const seconds = pad(Math.floor((milliseconds / 1000) % 60));
-    const minutes = pad(Math.floor((milliseconds / (1000 * 60)) % 60));
-    const hours = pad(Math.floor((milliseconds / (1000 * 60 * 60)) % 24));
- const days = pad(Math.floor(milliseconds / (1000 * 60 * 60 * 24)));
-    return {
-        days: days,
-        hours: hours,
-        minutes: minutes,
-        seconds: seconds,
-    };
+  const milliseconds = Math.abs(time);
+  const seconds = pad(Math.floor((milliseconds / 1000) % 60));
+  const minutes = pad(Math.floor((milliseconds / (1000 * 60)) % 60));
+  const hours = pad(Math.floor((milliseconds / (1000 * 60 * 60)) % 24));
+  const days = pad(Math.floor(milliseconds / (1000 * 60 * 60 * 24)));
+  return {
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
 }
-
-
-
-
-
-
-
-
-
-
