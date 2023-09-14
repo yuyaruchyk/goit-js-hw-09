cb0276ed31b060f9ca17692ba2626fc4228fbd8d
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-09-14 12:30:06 +0200

    upd
    upd

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+const form = document.querySelector('.form');
+
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    const shouldResolve = Math.random() > 0.3;
+                    if (shouldResolve) {
+                        resolve({ position, delay });
+                    } else {
+                        reject({ position, delay });
+                    }
+                }, delay);
+            });
+        }
+
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            const delay = parseInt(event.currentTarget.delay.value);
+            const step = parseInt(event.currentTarget.step.value);
+            const amount = parseInt(event.currentTarget.amount.value);
+
+            for (let i = 1; i <= amount; i++) {
+                const position = i;
+                const promiseDelay = delay + (i - 1) * step;
+
+                createPromise(position, promiseDelay)
+                    .then(({ position, delay }) => {
+                        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+                    })
+                    .catch(({ position, delay }) => {
+                        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+                    });
+            }
+        });
+
+
